c51d7ce1be80f181a6dfc1b059b2407b153768c5
Sharon Amelinda <sharonamelindaa@gmail.com>
2024-10-06 00:53:45 +0700

    Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./public/**/*.{html,js,css}"
+    "./public/**/*.{html,js,css}",
+    "./src/**/*.{html,js,css}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        'lightblue': '#f1f5fa',
+        'thin-dark': '#222534',
+        'stars': '#FFDE70'
+      }
+    },
   },
   plugins: [],
 }
